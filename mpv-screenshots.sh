59ea1f7c9c8df2a4c758bf146638b623354ed76e
Kcchouette <kcchouette@gmail.com>
2016-11-27 20:24:06 +0100

    Use percent in place of time
    to take screenshot.

--- mpv-screenshots.sh
+++ mpv-screenshots.sh
@@ -2,7 +2,7 @@
 
 ### Global variables
 declare -r file="$1"
-declare -r filename="$(basename "$file")"
+declare -r filename="$(basename "${file}")"
 
 ### Error handling
 if [ -z "${file}" ]; 
@@ -20,35 +20,22 @@ else
   declare -ri NUM_OF_SCREENSHOTS='9'
 fi
 
-# Get the total length of the video in seconds.
-declare -i total_length="$(mpv "$file" --quiet --vo null --ao null --frames 1 --term-playing-msg 'duration=${=duration}' | grep 'duration' | cut -d '=' -f2 | cut -d '.' -f1)"
-
-# Remove 4 seconds from the video so that it doesn't take screenshot at the ends.
-let total_length-='4'
-
-# Add 1 to the div number, like that the image is not at the end of the video
-# else, for 1 picture total_length/1 = total_length, so the end
-# with that, it'll be total_length/2
-declare -i DIV_NUMBER="${NUM_OF_SCREENSHOTS}"
-let DIV_NUMBER+='1'
-
-# time_slice: At which time interval should mpv take screenshots.
-let time_slice="${total_length}/${DIV_NUMBER}"
-
-# time_at: When should mplayer take screenshots.
-time_at=${time_slice};
+# Define begin percentage
+# 1 screen  -> 50/100                 = 100 / 2
+# 2 screens -> 33/100 67/100          = 100 / 3
+# 3 screens -> 25/100 50/100 75/100   = 100 / 4
+# ...
+declare startPercent="$((100/$((${NUM_OF_SCREENSHOTS}+1))))"
 
 # Looping to take screenshots.
 declare -i i
-for i in $(seq 1 "$NUM_OF_SCREENSHOTS")
+for i in $(seq 1 "${NUM_OF_SCREENSHOTS}")
 do
 
   # Take the screenshot.
-  declare name="${filename%.*}_${time_at}.png"
-  mpv --really-quiet --no-audio --vo=image --start=${time_at} --frames=1 "$file" -o "$name"
-
-  # Increment to the next time slice.
-  let time_at+="${time_slice}"
+  declare currentPercent="$((${startPercent}*$i))"
+  declare name="${filename%.*}_${currentPercent}_percent.png"
+  mpv --really-quiet --no-audio --no-sub --start="${currentPercent}%" --frames=1 "${file}" -o "${name}"
 
 done
 
