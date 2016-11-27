673c09c07ca79579bb66f5040b8b5b6272fdf1d4
Kcchouette <kcchouette@gmail.com>
2016-11-27 13:57:23 +0100

    update mpv-screenshots and improve it :)
    Thanks!

--- mpv-screenshots.sh
+++ mpv-screenshots.sh
@@ -1,55 +1,54 @@
 #!/usr/bin/env bash
 
 ### Global variables
-filename="$1"
+declare -r file="$1"
+declare -r filename="$(basename "$file")"
 
 ### Error handling
-if [ -z "${filename}" ]; 
+if [ -z "${file}" ]; 
 then
     echo "ERROR: No video file supplied. Please enter a video file as argument."
     exit 1;
 fi
 
-NUM_OF_SCREENSHOTS=9
 if [ ! -z "$2" ]; 
 then
-  NUM_OF_SCREENSHOTS=$2
+  declare -ri NUM_OF_SCREENSHOTS="$2"
   echo "WARNING: Overwrite default number of screenshots to ${NUM_OF_SCREENSHOTS}."
   sleep 3s
+else
+  declare -ri NUM_OF_SCREENSHOTS='9'
 fi
 
 # Get the total length of the video in seconds.
-#  Use mplayer to display the info of the video and then get the value of ID_LENGTH, the total number of seconds of the video.
-total_length=$(sh mpv_identify.sh -frames 0 -vc null -vo null -ao null "$filename"  | grep length | sed 's/length=//' | sed 's/\..*//')
-# Reference https://github.com/mpv-player/mpv/blob/master/TOOLS/mpv_identify.sh
+declare -i total_length="$(mpv "$file" --quiet --vo null --ao null --frames 1 --term-playing-msg 'duration=${=duration}' | grep 'duration' | cut -d '=' -f2 | cut -d '.' -f1)"
 
 # Remove 4 seconds from the video so that it doesn't take screenshot at the ends.
-let total_length-=4
+let total_length-='4'
 
 # Add 1 to the div number, like that the image is not at the end of the video
 # else, for 1 picture total_length/1 = total_length, so the end
 # with that, it'll be total_length/2
-DIV_NUMBER=${NUM_OF_SCREENSHOTS}
-let DIV_NUMBER+=1
+declare -i DIV_NUMBER="${NUM_OF_SCREENSHOTS}"
+let DIV_NUMBER+='1'
 
-# time_slice: At which time interval should mplayer take screenshots.
-let time_slice=${total_length}/${DIV_NUMBER}
+# time_slice: At which time interval should mpv take screenshots.
+let time_slice="${total_length}/${DIV_NUMBER}"
 
 # time_at: When should mplayer take screenshots.
 time_at=${time_slice};
 
 # Looping to take screenshots.
-for ((i=1; i <= NUM_OF_SCREENSHOTS ; i++))
+declare -i i
+for i in $(seq 1 "$NUM_OF_SCREENSHOTS")
 do
 
   # Take the screenshot.
-  #mplayer -loop 1 -nosound -frames 1 -ss ${time_at} -vo png:z=9 ${filename}
-  mpv --really-quiet --no-audio --vo=image --start=${time_at} --frames=1 "$filename" 
-  name="${filename%.*}_${time_at}.jpg"
-  mv 00000001.jpg "$name"
+  declare name="${filename%.*}_${time_at}.png"
+  mpv --really-quiet --no-audio --vo=image --start=${time_at} --frames=1 "$file" -o "$name"
 
   # Increment to the next time slice.
-  let time_at+=${time_slice}
+  let time_at+="${time_slice}"
 
 done
 
